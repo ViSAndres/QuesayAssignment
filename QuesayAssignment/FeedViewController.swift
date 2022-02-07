084fa3cdd2d8d5ab6857bd7da90941fc7abfc944
Carlos Neira <carlosneira1997@gmail.com>
2022-02-07 18:02:53 -0500

    Adding tableView to view controller

--- QuesayAssignment/FeedViewController.swift
+++ QuesayAssignment/FeedViewController.swift
@@ -7,14 +7,24 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var feedTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 
     /*
     // MARK: - Navigation
